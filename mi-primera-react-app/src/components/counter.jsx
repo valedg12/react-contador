6b2d82c0a7ac8549eaca02cf3c93fda868c53101
Valentina De Gaetano <valentinapugliesedegaetano@gmail.com>
2025-08-25 22:59:41 -0300

    evitar que el contador baje de 0

--- mi-primera-react-app/src/components/counter.jsx
+++ mi-primera-react-app/src/components/counter.jsx
@@ -4,12 +4,12 @@ import "./Counter.css";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  return (
+return (
     <div className="counter-container">
       <h1 className="counter-title">Contador</h1>
       <p className="counter-value">{count}</p>
       <div className="counter-buttons">
-        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(count > 0 ? count - 1 : 0)}>-</button>
         <button onClick={() => setCount(0)}>Reset</button>
         <button onClick={() => setCount(count + 1)}>+</button>
       </div>
