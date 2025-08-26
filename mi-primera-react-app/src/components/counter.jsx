0f9fe9bcb25637e541005c840c6d17eca9613150
Valentina De Gaetano <valentinapugliesedegaetano@gmail.com>
2025-08-25 22:55:57 -0300

    Primer commit: contador en React

---
+++ mi-primera-react-app/src/components/counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./Counter.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="counter-container">
+      <h1 className="counter-title">Contador</h1>
+      <p className="counter-value">{count}</p>
+      <div className="counter-buttons">
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
